34101c24aa06cb6b03d78cb2b3fa97219001a5a5
YarmUI <yarmui.gi@gmail.com>
2013-05-05 05:01:18 +0900

    typo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  redis_volatile: { port: 6380, hots: 'localhost', db: 0 },
-  redis:          { port: 6379, hots: 'localhost', db: 0 },
+  redis_volatile: { port: 6380, host: 'localhost', db: 0 },
+  redis:          { port: 6379, host: 'localhost', db: 0 },
   ttl: 3600,
   port: 3000,
   jobqueue_key:   'jobqueue'
